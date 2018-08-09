03ea3c432ab0e3a62b0e7a93ff5269fe91600727
Rishav Bhagat <rishavb123@gmail.com>
2018-08-09 12:00:29 -0400

    connected firebase project

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let config = {
+    apiKey: "AIzaSyCVLw22dTJ3O9i-42mDjKVzvB8wlMFdukU",
+    authDomain: "rbframework.firebaseapp.com",
+    databaseURL: "https://rbframework.firebaseio.com",
+    projectId: "rbframework",
+    storageBucket: "rbframework.appspot.com",
+    messagingSenderId: "644529138125"
+};
+firebase.initializeApp(config);
